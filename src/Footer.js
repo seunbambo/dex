8f1a7a5750ea672dc7587cc14e73a12119a7de92
Oluwaseun Adebambo <seun.bambo@nownow.ng>
2021-02-09 03:14:17 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+  From  <a href="http://technifiedsolutions.com.ng/" style={{color: 'white'}}>Technified Solutions</a>, World Class Blockchain Solution Deployer
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
